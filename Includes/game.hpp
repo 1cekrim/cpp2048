dc6b5a2cddc4302114b8f74493cfab81f20d37f3
HyeonSu Kim <rla92233@naver.com>
2019-10-11 16:17:04 +0900

    impl: Implement DrawBoard

---
+++ Includes/game.hpp
@@ -0,0 +1,24 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include "board.hpp"
+
+#include <iosfwd>
+#include <vector>
+
+namespace Game
+{
+class Game
+{
+ public:
+    Game(std::size_t height, std::size_t width);
+    void DrawBoard(std::ostream& os) const;
+    void CreateBlockRandomPosition();
+
+ private:
+    std::size_t m_height, m_width;
+    Board::Board m_board;
+};
+}  // namespace Game
+
+#endif

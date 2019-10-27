3ac3acebb7b9c0321b57e790066636f69e806883
HyeonSu Kim <rla92233@naver.com>
2019-10-27 15:40:43 +0900

    impl: Implement option

--- Includes/option.hpp
+++ Includes/option.hpp
@@ -1,18 +1,13 @@
 #ifndef OPTION_H
 #define OPTION_H
 
-#include "enums.hpp"
-
 #include <cstddef>
-#include <iostream>
 
+#include "enums.hpp"
 namespace Option
 {
-static std::size_t option;
-void ToggleOption(std::size_t op)
-{
-    Option::option ^= 1LL << static_cast<std::size_t>(op - 1);
-}
+void ToggleOption(std::size_t op);
+bool CheckOption(OptionEnum op);
 }  // namespace Option
 
 #endif
